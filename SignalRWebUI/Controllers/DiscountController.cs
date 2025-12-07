a95eac1aff19059dbf11a69465220133050bb622
furkanturker <furkanturker.dev@gmail.com>
2025-12-07 22:59:59 +0300

    Discount işlemleri tamamlandı

---
+++ SignalRWebUI/Controllers/DiscountController.cs
@@ -0,0 +1,94 @@
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.Dtos.DiscountDto;
+
+namespace SignalRWebUI.Controllers;
+
+public class DiscountController : Controller
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public DiscountController(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    // GET
+    public async Task<IActionResult> Index()
+    {
+        var client = _httpClientFactory.CreateClient();
+        var responsemessage = await client.GetAsync("http://localhost:5013/api/Discount");
+        if (responsemessage.IsSuccessStatusCode)
+        {
+            var jsonData = await responsemessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultDiscountDto>>(jsonData);
+            return View(values);
+        }
+
+        return View(new List<ResultDiscountDto>());
+    }
+
+    [HttpGet]
+    public IActionResult CreateDiscount()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateDiscount(CreateDiscountDto createDiscountDto)
+    {
+        var client = _httpClientFactory.CreateClient();
+        var JsonData = JsonConvert.SerializeObject(createDiscountDto);
+        StringContent stringContent = new StringContent(JsonData, Encoding.UTF8, "application/json");
+        var responsemessage = await client.PostAsync("http://localhost:5013/api/Discount", stringContent);
+        if (responsemessage.IsSuccessStatusCode)
+        {
+            return RedirectToAction("Index");
+        }
+
+        return View();
+    }
+
+    public async Task<IActionResult> DeleteDiscount(int id)
+    {
+        var client = _httpClientFactory.CreateClient();
+        var responsemessage = await client.DeleteAsync($"http://localhost:5013/api/Discount/{id}");
+        if (responsemessage.IsSuccessStatusCode)
+        {
+            return RedirectToAction("Index");
+        }
+
+        return View();
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> UpdateDiscount(int id)
+    {
+        var client = _httpClientFactory.CreateClient();
+        var responsemessage = await client.GetAsync($"http://localhost:5013/api/Discount/{id}");
+        if (responsemessage.IsSuccessStatusCode)
+        {
+            var JsonData = await responsemessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<UpdateDiscountDto>(JsonData);
+            return View(values);
+        }
+
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> UpdateDiscount(UpdateDiscountDto updateDiscountDto)
+    {
+        var client = _httpClientFactory.CreateClient();
+        var jsonData = JsonConvert.SerializeObject(updateDiscountDto);
+        StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+        var responsemessage = await client.PutAsync($"http://localhost:5013/api/Discount/", stringContent);
+        if (responsemessage.IsSuccessStatusCode)
+        {
+            return RedirectToAction("Index");
+        }
+
+        return View();
+    }
+}
--- SignalRWebUI/obj/Debug/net8.0/SignalRWebUI.AssemblyInfo.cs
+++ SignalRWebUI/obj/Debug/net8.0/SignalRWebUI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SignalRWebUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f14d9083bee5bccfe029e61e539dd121833e50c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb50a75f0cf0d2c44e3c02937a23961d83481217")]
 [assembly: System.Reflection.AssemblyProductAttribute("SignalRWebUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SignalRWebUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
