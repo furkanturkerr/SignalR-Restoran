a5bdc514871f9e6ae4ab7be6a10727ea5945fdcc
furkanturker <furkanturker.dev@gmail.com>
2025-12-12 22:16:45 +0300

    Rezervasyon verme sayfası tamamlandı

--- BusinessLayer/obj/Debug/net8.0/BusinessLayer.AssemblyInfo.cs
+++ BusinessLayer/obj/Debug/net8.0/BusinessLayer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65f0904c1aec386175d83487ff77f5878c304ee7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+343f3b8af2a132b747996cda17cc2db5503bb998")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ SignalRWebUI/Controllers/BookATableController.cs
@@ -0,0 +1,38 @@
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.Dtos.BookingDto;
+
+namespace SignalRWebUI.Controllers;
+
+public class BookATableController : Controller
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public BookATableController(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View();
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> Index(CreateBookingDto createBookingDto)
+    {
+        var client = _httpClientFactory.CreateClient();
+        var JsonData = JsonConvert.SerializeObject(createBookingDto);
+        StringContent stringContent = new StringContent(JsonData, Encoding.UTF8, "application/json");
+        var responsemessage = await client.PostAsync("http://localhost:5013/api/Booking", stringContent);
+        if (responsemessage.IsSuccessStatusCode)
+        {
+            return RedirectToAction("Index");
+        }
+
+        return View();
+    }
+}
